4a87f3a2de5494e6287625628615be1975b1e5a9
Maciej Urban <maciej-urban@outlook.com>
2023-08-22 19:42:11 +0200

    feat: allow to cancel selection or choose dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use std::env;
+use std::{
+    io::{BufRead, BufReader},
+    process::{Command, Stdio},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let home_dir = env::var("HOME").unwrap();
+    let fd_output = Command::new("fd")
+        .args([
+            "--type",
+            "d",
+            "-a",
+            "--hidden",
+            "--exclude",
+            ".git",
+            ".",
+            &home_dir,
+        ])
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to execute fd");
+
+    let mut fzf_tmux = Command::new("fzf-tmux")
+        .args(["-p", "--reverse"])
+        .stdin(Stdio::from(fd_output.stdout.unwrap()))
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to execute fzf-tmux");
+
+    fzf_tmux.wait().unwrap();
+
+    let mut final_output = String::new();
+    if let Some(stdout) = fzf_tmux.stdout.take() {
+        let mut bufread = BufReader::new(stdout);
+        let mut buf = String::new();
+
+        while let Ok(n) = bufread.read_line(&mut buf) {
+            if n > 0 {
+                final_output = buf.trim().into();
+            } else {
+                break;
+            }
+        }
+    }
+
+    if final_output.is_empty() {
+        final_output.push('.');
+    }
+
+    println!("{}", final_output);
 }
